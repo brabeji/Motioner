cc90ee11c15cff029d4443cf7e73a0e65b7e4355
frantatoman <franta.tomanu@gmail.com>
2017-11-19 20:33:40 +0100

    Base elements position y offset computing

--- src/js/motioner.js
+++ src/js/motioner.js
@@ -8,6 +8,10 @@ var elements = [];
 var Motioner = {
 	
   elements: [],
+  elementsDone: [],
+  elementsQueue: [],
+
+  onetime: false,             // animation happend only one-time
   
   init: function(){
 
@@ -16,25 +20,59 @@ var Motioner = {
 
     for (var node of elements) {
    		// add general motioner classes
-      node.className += " mo mo-in";
-      
+      node.className += " mo";
       // add specific animation, delays and other setups;
       node.className += " " + node.getAttribute('data-mo'); 
+
+      this.elementsQueue.push({
+        y: this.getPosition(node),    // add default position
+        node: node                      // add node
+      });
+
     }
 
-    addEventListener('resize', function(e){
-      console.log("Resize");
-      console.log(e);
-    });
+    var self = this;
 
-    addEventListener('scroll', function(e){
-      console.log("Scroll");
-      console.log(e);
-    });
+    addEventListener('resize', function(e){ self.update(); });
+    addEventListener('scroll', function(e){ self.update(); });
 
+    self.update();
+  },
+
+  getPosition: function(element) {
+      var yPosition = 0;
+
+      while(element) {
+          yPosition += (element.offsetTop - element.clientTop);
+          element = element.offsetParent;
+      }
+
+      return yPosition;
   },
 
 
+  update: function(){
+
+    var point = window.innerHeight/2;
+    var offset = window.pageYOffset || document.documentElement.scrollTop;
+
+    offset += point;
+
+    for(var node of this.elementsQueue){
+
+      if(offset > node.y){
+        
+        if(node.node.className.indexOf('mo-in') < 0){
+          node.node.className += " mo-in";
+        }
+
+      }else{
+        node.node.className = node.node.className.replace(" mo-in", "");
+      }
+
+    }
+
+  }
 
 
   
